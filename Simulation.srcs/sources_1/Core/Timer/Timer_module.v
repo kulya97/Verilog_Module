60b66f3a51adfbcc69c421cc32ebd889b16565c9
kulya97 <1942186814@qq.com>
2023-05-29 17:04:55 +0800

    修复定时不准确的bug

--- Simulation.srcs/sources_1/Core/Timer/Timer_module.v
+++ Simulation.srcs/sources_1/Core/Timer/Timer_module.v
@@ -23,12 +23,13 @@
 module Timer_module (
     input             clk,
     input             rst_n,
-    input      [15:0] app_wait_ms,
-    input             app_en,
-    output reg        app_ack
+    input      [31:0] I_app_wait_us,
+    input             I_app_req,
+    output reg        O_app_busy,
+    output reg        O_app_ack
 );
   parameter CLK_FRE = 50;
-  parameter MS_CNT = CLK_FRE * 1000;  //50*1000
+  parameter US_CNT = CLK_FRE;  //50*1000
   parameter LOOP = 1;
 
 
@@ -46,25 +47,25 @@ module Timer_module (
   reg [31:0] state_clk_cnt;
   always @(posedge clk, negedge rst_n) begin
     if (!rst_n) state_clk_cnt <= 'd0;
-    else if (STATE_NEXT != STATE_CURRENT || state_clk_cnt == MS_CNT - 1) state_clk_cnt <= 'd0;
+    else if (STATE_NEXT != STATE_CURRENT || state_clk_cnt == US_CNT - 1) state_clk_cnt <= 'd0;
     else state_clk_cnt <= state_clk_cnt + 1'd1;
   end
-  reg [15:0] state_ms;
+  reg [31:0] state_us;
   always @(posedge clk, negedge rst_n) begin
-    if (!rst_n) state_ms <= 'd0;
-    else if (STATE_NEXT != STATE_CURRENT) state_ms <= 'd0;
-    else if (state_clk_cnt == MS_CNT - 1) state_ms <= state_ms + 1'd1;
-    else state_ms <= state_ms;
+    if (!rst_n) state_us <= 'd0;
+    else if (STATE_NEXT != STATE_CURRENT) state_us <= 'd0;
+    else if (state_clk_cnt == US_CNT - 1) state_us <= state_us + 1'd1;
+    else state_us <= state_us;
   end
   /**************************转移状态****************************/
   always @(*) begin
     case (STATE_CURRENT)
       S_IDLE: begin
-        if (app_en) STATE_NEXT = S_WAIT;
+        if (I_app_req) STATE_NEXT = S_WAIT;
         else STATE_NEXT = S_IDLE;
       end
       S_WAIT: begin
-        if (state_ms == app_wait_ms - 1) STATE_NEXT = S_ACK;
+        if (state_us == I_app_wait_us - 1) STATE_NEXT = S_ACK;
         else STATE_NEXT = S_WAIT;
       end
       S_ACK: begin
@@ -80,8 +81,15 @@ module Timer_module (
 
   /**************************状态输出****************************/
   always @(posedge clk, negedge rst_n) begin
-    if (!rst_n) app_ack <= 1'd0;
-    else if (STATE_CURRENT == S_ACK) app_ack <= 1'd1;
-    else app_ack <= 1'd0;
+    if (!rst_n) O_app_ack <= 1'd0;
+    else if (STATE_CURRENT == S_ACK) O_app_ack <= 1'd1;
+    else O_app_ack <= 1'd0;
+  end
+
+  /**************************状态输出****************************/
+  always @(posedge clk, negedge rst_n) begin
+    if (!rst_n) O_app_busy <= 1'd0;
+    else if (STATE_CURRENT == S_WAIT) O_app_busy <= 1'd1;
+    else O_app_busy <= 1'd0;
   end
 endmodule
