d81e37a1646183a4ab6f8b8b6da47e0e8ebf62f3
kulya97 <1942186814@qq.com>
2023-06-01 08:57:14 +0800

    no message

--- Simulation.srcs/sources_1/Core/Timer/Timer_module.v
+++ Simulation.srcs/sources_1/Core/Timer/Timer_module.v
@@ -8,8 +8,8 @@
 // Module Name: Timer_module
 // Project Name: 
 // Target Devices: 
-// Tool Versions: 
-// Description: 用于定时提供一个触发信号，可循环产生可只产生一次；
+// Tool Versions: 2023.05.09 v1.1
+// Description: 用于定时提供一个触发信号;
 // 
 // Dependencies: 
 // 
@@ -17,37 +17,47 @@
 // Revision 0.01 - File Created
 // Additional Comments:
 // 
+
+// Timer_module #(
+//     .CLK_FRE ( CLK_FRE ))
+//  u_Timer_module (
+//     .clk                     ( clk                   ),
+//     .rst_n                   ( rst_n                 ),
+//     .I_app_timer_us           ( I_app_timer_us  [31:0] ),
+//     .I_app_req               ( I_app_req             ),
+
+//     .O_app_busy              ( O_app_busy            ),
+//     .O_app_ack               ( O_app_ack             )
+// );
 //////////////////////////////////////////////////////////////////////////////////
 
 
 module Timer_module (
     input             clk,
     input             rst_n,
-    input      [31:0] I_app_wait_us,
+    input      [31:0] I_app_timer_us,
     input             I_app_req,
     output reg        O_app_busy,
     output reg        O_app_ack
 );
   parameter CLK_FRE = 50;
-  parameter US_CNT = CLK_FRE;  //50*1000
-  parameter LOOP = 1;
+  localparam US_CNT = CLK_FRE;  //50
 
 
   /**************************同步状态****************************/
-  reg [4:0] STATE_CURRENT;
-  reg [4:0] STATE_NEXT;
-  localparam S_IDLE = 5'd0;  //空闲，fifo无数据状态
-  localparam S_WAIT = 5'd1;  //正常间隔
-  localparam S_ACK = 5'd2;
-  localparam S_DONE = 5'd3;
+  reg [1:0] STATE_CURRENT;
+  reg [1:0] STATE_NEXT;
+  localparam S_IDLE = 2'd0;  //
+  localparam S_WAIT = 2'd1;  //
+  localparam S_ACK = 2'd2;
   always @(posedge clk, negedge rst_n) begin
     if (!rst_n) STATE_CURRENT <= S_IDLE;
     else STATE_CURRENT <= STATE_NEXT;
   end
-  reg [31:0] state_clk_cnt;
+  reg [7:0] state_clk_cnt;
   always @(posedge clk, negedge rst_n) begin
-    if (!rst_n) state_clk_cnt <= 'd0;
-    else if (STATE_NEXT != STATE_CURRENT || state_clk_cnt == US_CNT - 1) state_clk_cnt <= 'd0;
+    if (!rst_n) state_clk_cnt <= 8'd0;
+    else if (STATE_NEXT != STATE_CURRENT || state_clk_cnt == US_CNT - 1) state_clk_cnt <= 8'd0;
     else state_clk_cnt <= state_clk_cnt + 1'd1;
   end
   reg [31:0] state_us;
@@ -58,6 +68,8 @@ module Timer_module (
     else state_us <= state_us;
   end
   /**************************转移状态****************************/
+  wire W_ACK;
+  assign W_ACK = (state_us == I_app_timer_us - 1 && state_clk_cnt == US_CNT - 2);
   always @(*) begin
     case (STATE_CURRENT)
       S_IDLE: begin
@@ -65,15 +77,11 @@ module Timer_module (
         else STATE_NEXT = S_IDLE;
       end
       S_WAIT: begin
-        if (state_us == I_app_wait_us - 1) STATE_NEXT = S_ACK;
+        if (W_ACK) STATE_NEXT = S_IDLE;
         else STATE_NEXT = S_WAIT;
       end
       S_ACK: begin
-        STATE_NEXT = S_DONE;
-      end
-      S_DONE: begin
-        if (LOOP) STATE_NEXT = S_IDLE;
-        else STATE_NEXT = S_DONE;
+        STATE_NEXT = S_IDLE;
       end
       default: STATE_NEXT = S_IDLE;
     endcase
@@ -82,7 +90,7 @@ module Timer_module (
   /**************************状态输出****************************/
   always @(posedge clk, negedge rst_n) begin
     if (!rst_n) O_app_ack <= 1'd0;
-    else if (STATE_CURRENT == S_ACK) O_app_ack <= 1'd1;
+    else if (W_ACK) O_app_ack <= 1'd1;
     else O_app_ack <= 1'd0;
   end
 
