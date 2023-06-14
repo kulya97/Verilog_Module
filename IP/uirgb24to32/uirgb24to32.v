afde5cbe40fc2a1c2eec1b4c0341fc4d48dfd70a
kulya97 <1942186814@qq.com>
2023-06-14 15:23:57 +0800

    no message

---
+++ IP/uirgb24to32/uirgb24to32.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+/*******************************MILIANKE*******************************
+*Company : MiLianKe Electronic Technology Co., Ltd.
+*WebSite:https://www.milianke.com
+*TechWeb:https://www.uisrc.com
+*tmall-shop:https://milianke.tmall.com
+*jd-shop:https://milianke.jd.com
+*taobao-shop1: https://milianke.taobao.com
+*Create Date: 2021/10/15
+*Module Name:dprgb24to36
+*File Name:dprgb24to36.v
+*Description: 
+*The reference demo provided by Milianke is only used for learning. 
+*We cannot ensure that the demo itself is free of bugs, so users 
+*should be responsible for the technical problems and consequences
+*caused by the use of their own products.
+*Copyright: Copyright (c) MiLianKe
+*All rights reserved.
+*Revision: 1.0
+*Signal description
+*1) _i input
+*2) _o output
+*3) _n activ low
+*4) _dg debug signal 
+*5) _r delay or register
+*6) _s state mechine
+*********************************************************************/
+module rgb24to32
+(
+input  [23:0] rgb24,
+output [31:0] rgb32
+);
+
+assign rgb32= {8'hff,rgb24[23:0]};
+
+endmodule
