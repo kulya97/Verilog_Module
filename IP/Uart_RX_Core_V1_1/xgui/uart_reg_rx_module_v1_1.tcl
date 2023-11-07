d0edbc611c8905ae688f659881fb27b4cde4d3a2
kulya97 <1942186814@qq.com>
2023-11-07 11:25:32 +0800

    修复：等待fifo复位完成后再工作

--- IP/Uart_RX_Core_V1_1/xgui/uart_reg_rx_module_v1_1.tcl
+++ IP/Uart_RX_Core_V1_1/xgui/uart_reg_rx_module_v1_1.tcl
@@ -1,13 +1,10 @@
 # Definitional proc to organize widgets for parameters.
 proc init_gui { IPINST } {
   ipgui::add_param $IPINST -name "Component_Name"
-  #Adding Page
-  set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
-  ipgui::add_param $IPINST -name "BPS" -parent ${Page_0}
-  ipgui::add_param $IPINST -name "CLK_FRE" -parent ${Page_0}
-  ipgui::add_param $IPINST -name "IDLE_CYCLE" -parent ${Page_0}
-  ipgui::add_param $IPINST -name "REG_WIDTH" -parent ${Page_0}
-
+  ipgui::add_param $IPINST -name "CLK_FRE"
+  ipgui::add_param $IPINST -name "BPS"
+  ipgui::add_param $IPINST -name "IDLE_CYCLE"
+  ipgui::add_param $IPINST -name "REG_WIDTH"
 
 }
 
--- IP/Uart_TX_Core_V1_1/src/uart_reg_tx_module.v
+++ IP/Uart_TX_Core_V1_1/src/uart_reg_tx_module.v
@@ -24,7 +24,7 @@ module uart_reg_tx_module #(
 ) (
     input                  clk,            //system clock 50Mhz on board
     input                  rst_n,          //reset ,low active
-    output                 uart_tx_port1,
+    output                 uart_tx_port,
     input  [REG_WIDTH-1:0] uart_tx_reg,
     input                  uart_tx_valid,
     output                 uart_tx_ready
@@ -39,6 +39,9 @@ module uart_reg_tx_module #(
   wire                 fifo_empty;  // output wire empty 
   wire                 fifo_valid;
   wire                 fifo_prog_full;
+
+  wire                 fifo_rd_rst_busy;
+  wire                 fifo_wr_rst_busy;
   /**********************************************/
   wire                 par_valid;  // input
   wire [REG_WIDTH-1:0] par_din;  // input
@@ -67,7 +70,7 @@ module uart_reg_tx_module #(
   assign tx_data_valid = ser_valid;
   assign rd_en         = tx_data_valid && tx_data_ready;
 
-  assign uart_tx_ready = !fifo_prog_full;  //fifo 没满前一直读取数据到fifo中
+  assign uart_tx_ready = !(fifo_prog_full||fifo_rd_rst_busy||fifo_wr_rst_busy);  //fifo 没满前一直读取数据到fifo中,并且需要复位完成
   /*******************************************************************/
 
   xpm_fifo_sync #(
@@ -89,18 +92,20 @@ module uart_reg_tx_module #(
       .SIM_ASSERT_CHK     (0),          // 0=禁用仿真消息，1=启用仿真消息
       .WAKEUP_TIME        (0)           // 禁用sleep
   ) xpm_fifo_sync_inst (
-      .rst       (!rst_n),          // 1-bit input: fifo复位
-      .wr_clk    (clk),             // 1-bit input:写时钟
-      .wr_en     (fifo_wr_en),      // 1-bit input:写使能
-      .din       (fifo_din),        // data  input:写数据
-      .rd_en     (fifo_rd_en),      // 1-bit input:读使能
-      .dout      (fifo_dout),       // data  output读复位
-      .data_valid(fifo_valid),      // 1-bit output:数据有效
-      .wr_ack    (),                // 1-bit output:写响应
-      .prog_empty(),                // 1-bit output:快满标志位
-      .prog_full (fifo_prog_full),  // 1-bit output:快空标志位
-      .empty     (fifo_empty),      // 1-bit output:fifo空标志位
-      .full      (fifo_full)        // 1-bit output:fifo满标志位
+      .rst        (!rst_n),            // 1-bit input: fifo复位
+      .wr_clk     (clk),               // 1-bit input:写时钟
+      .wr_en      (fifo_wr_en),        // 1-bit input:写使能
+      .din        (fifo_din),          // data  input:写数据
+      .rd_en      (fifo_rd_en),        // 1-bit input:读使能
+      .dout       (fifo_dout),         // data  output读复位
+      .data_valid (fifo_valid),        // 1-bit output:数据有效
+      .wr_ack     (),                  // 1-bit output:写响应
+      .prog_empty (),                  // 1-bit output:快满标志位
+      .prog_full  (fifo_prog_full),    // 1-bit output:快空标志位
+      .rd_rst_busy(fifo_rd_rst_busy),  // 1-bit output: 写入复位忙：活动高指示FIFO当前处于复位状态
+      .wr_rst_busy(fifo_wr_rst_busy),  // 1-bit output: 读取复位忙：活动高指示FIFO当前处于复位状态
+      .empty      (fifo_empty),        // 1-bit output:fifo空标志位
+      .full       (fifo_full)          // 1-bit output:fifo满标志位
   );
 
   /*******************************************************************/
