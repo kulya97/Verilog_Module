22440c63a0aa850b09137379ddfcf2fa932b8048
kulya91 <1942186814@qq.com>
2024-01-19 14:29:38 +0800

    updata apb master

--- Simulation.srcs/sources_1/APB/APB_Master_Core.v
+++ Simulation.srcs/sources_1/APB/APB_Master_Core.v
@@ -22,35 +22,31 @@
 module APB_Master_Core #(
     parameter APB_DBIT = 32,
     parameter APB_ABIT = 32,
-    parameter RD_FLAG  = 2'b00,
-    parameter WR_FLAG  = 2'b11
+    parameter CMD_DBIT = 8,
+    parameter RD_FLAG  = 8'h00,
+    parameter WR_FLAG  = 8'hff
 ) (
     //-- user port
-    input                       i_cmd_rvld,
-    output                      o_cmd_rready,
-    input      [           1:0] i_cmd_rw,
-    input      [  APB_ABIT-1:0] i_cmd_addr,
-    input      [  APB_DBIT-1:0] i_cmd_wdata,
-    output reg [  APB_DBIT-1:0] o_cmd_rdata,
+    input                                       i_cmd_vld,
+    output reg                                  o_cmd_ready,
+    input      [APB_ABIT+APB_DBIT+CMD_DBIT-1:0] i_cmd_data,
+    output reg [                  APB_DBIT-1:0] o_cmd_rdata,
     //-- apb if
-`ifdef APB4
-    output     [           2:0] o_apb_prot,     //APB4 sign unused  000
-    output     [APB_DBIT/8-1:0] o_apb_strb,     //APB4 sign 
-`endif
-    //-- apb3 if
-    input                       i_apb_clk,
-    input                       i_apb_rstn,
-    output reg [  APB_ABIT-1:0] o_apb_addr,
-    output reg                  o_apb_write,
-    output reg                  o_apb_selx,
-    output reg                  o_apb_enable,
-    output reg [  APB_DBIT-1:0] o_apb_wdata,
-    input      [  APB_DBIT-1:0] i_apb_rata,
-    input                       i_apb_ready,
-    input                       i_apb_slverr
+    output     [                           2:0] o_apb_prot,    //APB4 sign unused  000
+    output     [                APB_DBIT/8-1:0] o_apb_strb,    //APB4 sign 
+    input                                       i_apb_clk,
+    input                                       i_apb_rstn,
+    output reg [                  APB_ABIT-1:0] o_apb_addr,
+    output reg                                  o_apb_write,
+    output reg                                  o_apb_selx,
+    output reg                                  o_apb_enable,
+    output reg [                  APB_DBIT-1:0] o_apb_wdata,
+    input      [                  APB_DBIT-1:0] i_apb_rdata,
+    input                                       i_apb_ready,
+    input                                       i_apb_slverr
 );
 
-  localparam CMD_WIDTH = APB_DBIT + APB_ABIT + 2;
+  localparam CMD_WIDTH = APB_DBIT + APB_ABIT + CMD_DBIT;
   //-- FSM state
   localparam S_IDLE = 3'b001;
   localparam S_SETUP = 3'b010;
@@ -60,16 +56,23 @@ module APB_Master_Core #(
   reg [          2:0] nxt_state;
 
   //-- data buf
-  reg                 start_flag;
   reg [CMD_WIDTH-1:0] cmd_in_buf;
   reg [ APB_DBIT-1:0] cmd_rd_data_buf;
   /*****************************************************************/
-`ifdef APB4
   assign o_apb_prot = 3'b000;  //APB4 sign unused  000
   assign o_apb_strb = o_apb_write ? 4'b0000 : 4'b1111;
-`endif
 
-  assign o_cmd_rready = i_apb_ready;
+
+
+
+  always @(posedge i_apb_clk or negedge i_apb_rstn) begin
+    if (!i_apb_rstn) begin
+      o_cmd_ready <= 1'b0;
+    end else if (i_cmd_vld && !o_cmd_ready && cur_state == S_IDLE) o_cmd_ready <= 1'b1;
+    else o_cmd_ready <= 1'b0;
+  end
+
+
   /*****************************************************************/
   /*-----------------------------------------------
  --             update cmd_in_buf              --
@@ -77,21 +80,8 @@ module APB_Master_Core #(
   always @(posedge i_apb_clk or negedge i_apb_rstn) begin
     if (!i_apb_rstn) begin
       cmd_in_buf <= {(CMD_WIDTH) {1'b0}};
-    end else if (i_cmd_rvld && o_cmd_rready) begin
-      cmd_in_buf <= {i_cmd_rw, i_cmd_addr, i_cmd_wdata};
-    end
-  end
-
-  /*-----------------------------------------------
- --             start flag of transfer         --
------------------------------------------------*/
-  always @(posedge i_apb_clk or negedge i_apb_rstn) begin
-    if (!i_apb_rstn) begin
-      start_flag <= 1'b0;
-    end else if (i_cmd_rvld && o_cmd_rready) begin
-      start_flag <= 1'b1;
-    end else begin
-      start_flag <= 1'b0;
+    end else if (i_cmd_vld && o_cmd_ready) begin
+      cmd_in_buf <= i_cmd_data;
     end
   end
 
@@ -105,25 +95,27 @@ module APB_Master_Core #(
       cur_state <= nxt_state;
     end
   end
-
   /*-----------------------------------------------
  --               update next state            --
 -----------------------------------------------*/
   always @(*) begin
     case (cur_state)
       S_IDLE:
-      if (start_flag) begin
+      if (i_cmd_vld && o_cmd_ready) begin
         nxt_state = S_SETUP;
       end else begin
         nxt_state = S_IDLE;
       end
       S_SETUP: nxt_state = S_ACCESS;
       S_ACCESS:
-      if (!o_cmd_rready) begin
+      if (!i_apb_ready) begin  //-- 从机未准备好
         nxt_state = S_ACCESS;
-      end else if (start_flag) begin
-        nxt_state = S_SETUP;
-      end else if (!i_cmd_rvld && o_cmd_rready) begin
+        // end else if (i_cmd_vld && o_cmd_ready) begin  //-- 连续接收
+        //   nxt_state = S_SETUP;
+        // end else if (!i_cmd_vld && o_cmd_ready) begin  //-- 接收完成
+        //   nxt_state = S_IDLE;
+        // end
+      end else begin  //-- 接收完成
         nxt_state = S_IDLE;
       end
     endcase
@@ -139,22 +131,22 @@ module APB_Master_Core #(
       o_apb_enable <= 1'b0;
       o_apb_addr   <= {(APB_ABIT) {1'b0}};
       o_apb_wdata  <= {(APB_DBIT) {1'b0}};
-    end else if (nxt_state == S_IDLE) begin
+    end else if (cur_state == S_IDLE) begin
       o_apb_selx   <= 1'b0;
       o_apb_enable <= 1'b0;
-    end else if (nxt_state == S_SETUP) begin
+    end else if (cur_state == S_SETUP) begin
       o_apb_selx   <= 1'b1;
       o_apb_enable <= 1'b0;
-      o_apb_addr   <= cmd_in_buf[CMD_WIDTH-2-1:APB_DBIT];
+      o_apb_addr   <= cmd_in_buf[CMD_WIDTH-CMD_DBIT-1:APB_DBIT];
       //-- read
-      if (cmd_in_buf[CMD_WIDTH-1:CMD_WIDTH-2] == RD_FLAG) begin
+      if (cmd_in_buf[CMD_WIDTH-1:CMD_WIDTH-CMD_DBIT] == RD_FLAG) begin
         o_apb_write <= 1'b0;
       end  //-- write
       else begin
         o_apb_write <= 1'b1;
         o_apb_wdata <= cmd_in_buf[APB_DBIT-1:0];
       end
-    end else if (nxt_state == S_ACCESS) begin
+    end else if (cur_state == S_ACCESS) begin
       o_apb_enable <= 1'b1;
     end
   end
@@ -166,7 +158,7 @@ module APB_Master_Core #(
     if (!i_apb_rstn) begin
       cmd_rd_data_buf <= {(APB_DBIT) {1'b0}};
     end else if (i_apb_ready && o_apb_selx && o_apb_enable) begin
-      cmd_rd_data_buf <= i_apb_rata;
+      cmd_rd_data_buf <= i_apb_rdata;
     end
   end
 
--- Simulation.srcs/sources_1/APB/ISP_APB_UART_TOP.v
+++ Simulation.srcs/sources_1/APB/ISP_APB_UART_TOP.v
@@ -1,7 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
+// Company: NJUST
+// Engineer: huangwenjie
 // 
 // Create Date: 2024/01/17 16:51:36
 // Design Name: 
@@ -24,61 +24,99 @@ module ISP_APB_UART_TOP #(
     parameter APB_DBIT = 32,
     parameter APB_ABIT = 32
 ) (
-    input                       i_uart_rx,
-    output                      o_uart_tx,
-`ifdef APB4
-    output     [           2:0] o_apb_prot,    //APB4 sign unused  000
-    output     [APB_DBIT/8-1:0] o_apb_strb,    //APB4 sign unused
-`endif
-    //-- apb3 if
-    input                       i_apb_clk,
-    input                       i_apb_rstn,
-    output reg [  APB_ABIT-1:0] o_apb_addr,
-    output reg                  o_apb_write,
-    output reg                  o_apb_selx,
-    output reg                  o_apb_enable,
-    output reg [  APB_DBIT-1:0] o_apb_wdata,
-    input      [  APB_DBIT-1:0] i_apb_rata,
-    input                       i_apb_ready,
-    input                       i_apb_slverr
-
-
+    //-- UI PORT
+    input                   i_init_en,
+    output                  o_init_done,
+    //-- UART
+    input                   i_uart_rx,
+    output                  o_uart_tx,
+    output [           2:0] o_apb_prot,    //APB4 sign unused  000
+    output [APB_DBIT/8-1:0] o_apb_strb,    //APB4 sign unused
+    input                   i_apb_clk,
+    input                   i_apb_rstn,
+    output [  APB_ABIT-1:0] o_apb_addr,
+    output                  o_apb_write,
+    output                  o_apb_selx,
+    output                  o_apb_enable,
+    output [  APB_DBIT-1:0] o_apb_wdata,
+    input  [  APB_DBIT-1:0] i_apb_rdata,
+    input                   i_apb_ready,
+    input                   i_apb_slverr
 );
+  /********************************************************/
+  //-- cfg
+  wire        cfg_valid;
+  wire        cfg_ready;
+  wire [71:0] cfg_data;
+  wire        cfg_init_done;
+  //-- uart rx
+  wire        uart_ready;
+  wire        uart_valid;
+  wire [71:0] uart_data;
+  //-- apb master
+  wire        apb_cmd_vld;
+  wire        apb_cmd_ready;
 
+  wire [31:0] apb_cmd_rdata;
+  wire [71:0] apb_cmd_data;
+  /********************************************************/
+  assign apb_cmd_data = cfg_init_done ? uart_data : cfg_data;
+  assign apb_cmd_vld  = i_init_en && (cfg_valid || uart_valid);
+
+  assign cfg_ready    = i_init_en && !cfg_init_done && apb_cmd_ready;
+  assign uart_ready   = i_init_en && cfg_init_done && apb_cmd_ready;
+  assign o_init_done  = cfg_init_done;
+  /********************************************************/
   APB_Master_Core #(
       .APB_DBIT(APB_DBIT),
       .APB_ABIT(APB_ABIT),
-      .RD_FLAG (00),
-      .WR_FLAG (11)
+      .CMD_DBIT(8),
+      .RD_FLAG (8'h00),
+      .WR_FLAG (8'hff)
   ) APB_Master_Core_inst (
-      .i_cmd_vld    (i_cmd_vld),
-      .o_cmd_ready  (o_cmd_ready),
-      .i_cmd_rw     (i_cmd_rw),
-      .i_cmd_addr   (i_cmd_addr),
-      .i_cmd_wdata  (i_cmd_wdata),
-      .o_cmd_rd_data(o_cmd_rd_data),
-`ifdef APB4
-      .o_apb_prot   (o_apb_prot),     //APB4 sign unused  000
-      .o_apb_strb   (o_apb_strb),     //APB4 sign 
-`endif
-      .i_apb_clk    (i_apb_clk),
-      .i_apb_rstn   (i_apb_rstn),
-      .o_apb_addr   (o_apb_addr),
-      .o_apb_write  (o_apb_write),
-      .o_apb_selx   (o_apb_selx),
-      .o_apb_enable (o_apb_enable),
-      .o_apb_wdata  (o_apb_wdata),
-      .i_apb_rata   (i_apb_rata),
-      .i_apb_ready  (i_apb_ready),
-      .i_apb_slverr (i_apb_slverr)
+      .i_cmd_vld   (apb_cmd_vld),
+      .o_cmd_ready (apb_cmd_ready),
+      .i_cmd_data  (apb_cmd_data),
+      .o_cmd_rdata (apb_cmd_rdata),
+      .o_apb_prot  (o_apb_prot),     //APB4 sign unused  000
+      .o_apb_strb  (o_apb_strb),     //APB4 sign 
+      .i_apb_clk   (i_apb_clk),
+      .i_apb_rstn  (i_apb_rstn),
+      .o_apb_addr  (o_apb_addr),
+      .o_apb_write (o_apb_write),
+      .o_apb_selx  (o_apb_selx),
+      .o_apb_enable(o_apb_enable),
+      .o_apb_wdata (o_apb_wdata),
+      .i_apb_rdata (i_apb_rdata),
+      .i_apb_ready (i_apb_ready),
+      .i_apb_slverr(i_apb_slverr)
   );
-
-  uart_reg_rx_module_1 your_instance_name (
-      .clk          (i_apb_clk),      // input wire clk
-      .rst_n        (i_apb_rstn),     // input wire rst_n
-      .uart_rx_port (i_uart_rx),      // input wire uart_rx_port
-      .uart_rx_data (uart_rx_data),   // output wire [65 : 0] uart_rx_data
-      .uart_rx_ready(uart_rx_ready),  // input wire uart_rx_ready
-      .uart_rx_valid(uart_rx_valid)   // output wire uart_rx_valid
+  /********************************************************/
+  ISP_APB_REG_CFG #(
+      .APB_DBIT(APB_DBIT),
+      .APB_ABIT(APB_ABIT),
+      .CMD_DBIT(8)
+  ) ISP_APB_REG_CFG_inst (
+      .i_clk      (i_apb_clk),
+      .i_rstn     (i_apb_rstn),
+      .o_valid    (cfg_valid),
+      .i_ready    (cfg_ready),
+      .o_data     (cfg_data),
+      .o_init_done(cfg_init_done)
+  );
+  /********************************************************/
+  uart_reg_rx_module #(
+      .CLK_FRE   (10),       //Mhz
+      .BPS       (1000000),  //uart bps
+      .IDLE_CYCLE(20),       //idle time
+      .REG_WIDTH (72)
+  ) your_instance_name (
+      .clk          (i_apb_clk),   // input wire clk
+      .rst_n        (i_apb_rstn),  // input wire rst_n
+      .uart_rx_en   (1),
+      .uart_rx_port (i_uart_rx),   // input wire uart_rx_port
+      .uart_rx_data (uart_data),   // output wire [71 : 0] uart_rx_data
+      .uart_rx_ready(uart_ready),  // input wire uart_rx_ready
+      .uart_rx_valid(uart_valid)   // output wire uart_rx_valid
   );
 endmodule
